c1a95dcb5ffdd009e906fba1fb6fc228ef5f4981
Anup <anup19981998@gmail.com>
2026-03-22 15:48:49 +0530

    dynamic api url for prod

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,4 @@
-export const API = "http://localhost:8000/api";
+export const API = import.meta.env.VITE_API_URL || "http://localhost:8000/api";
 
 const apiFetch = {
   async post(path, body, token) {
